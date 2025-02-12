f34d2d527316b88194a727fc53773dc81c1fee65
Azri Shukor <azrishukor22@gmail.com>
2025-02-12 10:43:52 +0800

    Forcing all subfolders to be tracked

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+VITE_API_BASE_URL="https://ebooks-backend-production.up.railway.app"
+
 export default defineConfig({
   plugins: [react()],
 })
