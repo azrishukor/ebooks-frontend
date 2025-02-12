c18c4bacb8318713b378f9b1c0b7e1ab43a75309
Azri Shukor <azrishukor22@gmail.com>
2025-02-12 11:12:29 +0800

    Forcing all subfolders to be tracked

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-console.log("API Base URL:", API_BASE_URL); // Debugging
-
-
-// https://vite.dev/config/
-VITE_API_BASE_URL="https://ebooks-backend-production.up.railway.app"
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': process.env // Fix for Vercel
+    'import.meta.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL)
   }
 });
-
